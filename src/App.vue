936e0d82ca1adcf032fc6c586908b9bcb490b7f0
Natasha Buckham <natasha.buckham@softwire.com>
2024-11-20 14:20:23 +0000

    lesson 2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-  import CounterButton from './components/CounterButton.vue';
+  import CoffeePlan from './components/CoffeePlan.vue';
+  import {ref} from 'vue'
+
+  const plans = ref(["The Single", "The Curious", "The Addict", "The Hacker"])
 </script>
 
 <template>
-  <CounterButton />
-  <CounterButton />
-  <CounterButton />
+ <div class="content">
+  <h1 class="title">Coffee Plans</h1>
+
+  <h2 class="subtitle">We travel the world to source the very best single origin coffee for you</h2>
+
+  <div class="plans">
+    <CoffeePlan v-for="plan in plans" :key="plan" :name="plan"/>
+  </div>
+</div>
 </template>
 
 <style scoped>
---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,25 @@
+<script setup>
+    // defineProps(['name']) define props as an array
+    // const props = defineProps(['name']) assign to a constant so the props can be used inside script
+    
+    // best practice is to use an array and define the type
+    // defineProps({
+    //     name: String,
+    //     price: Number,
+    // })
+
+    // can also give the prop a default value and make it required
+    // this will throw an error in browser console if not followed
+    defineProps({
+     name: { type: String, default: 'Default Plan' },
+    //  price: { type: Number, required: true }
+    })
+</script>
+
+<template>
+    <div class="plan">
+      <div class="description">
+        <span class="title">{{ name }}</span>
+      </div>
+    </div>
+</template>
