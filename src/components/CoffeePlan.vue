720e289e238d930119ea43eaf02a035882af58f8
Natasha Buckham <natasha.buckham@softwire.com>
2024-11-20 15:05:40 +0000

    lesson 5

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import {ref} from 'vue'
     // defineProps(['name']) define props as an array
     // const props = defineProps(['name']) assign to a constant so the props can be used inside script
     
@@ -10,14 +11,21 @@
 
     // can also give the prop a default value and make it required
     // this will throw an error in browser console if not followed
-    defineProps({
+    const props = defineProps({
      name: { type: String, default: 'Default Plan' },
+     selected: { type: Boolean, default: false }
     //  price: { type: Number, required: true }
     })
+
+    const emit = defineEmits(['selected']) // define a custom event with defineEmits, returns an emit function which is saved in a const
+
+    function selectPlan() {
+        emit('selected', props.name) // use saved emit function to emit an event called selected and send a payload to parent
+    }
 </script>
 
 <template>
-    <div class="plan">
+    <div @click="selectPlan" class="plan" :class="{ 'active-plan': selected }">
       <div class="description">
         <span class="title">{{ name }}</span>
       </div>
--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -2,10 +2,25 @@
     import CoffeePlan from './CoffeePlan.vue';
     import {ref} from 'vue'
     const plans = ref(["The Single", "The Curious", "The Addict", "The Hacker"])
+
+    const selectedCoffeePlan = ref()
+
+    function handleSelectCoffeePlan(name) {
+        selectedCoffeePlan.value = name
+    }
 </script>
 
 <template>
     <div class="plans">
-    <CoffeePlan v-for="plan in plans" :key="plan" :name="plan"/>
-  </div>
+        {{ selectedCoffeePlan }}
+        <!-- listen for event with @ and call function when event occurs, payload is automatically passed to the function -->
+        <CoffeePlan 
+            v-for="plan in plans" 
+            :key="plan" 
+            :name="plan" 
+            :selected="plan === selectedCoffeePlan" 
+            @selected="handleSelectCoffeePlan" 
+        />
+        <!-- instead of doing a custom event with @selected, you could use @click and pass 'plan' (the name of the plan) -->
+    </div>
 </template>
