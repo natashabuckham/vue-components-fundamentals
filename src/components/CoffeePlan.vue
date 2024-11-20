3db36d01b00fca3c44567dab5c810d13acdacffb
Natasha Buckham <natasha.buckham@softwire.com>
2024-11-20 15:12:50 +0000

    lesson 6

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -12,14 +12,24 @@
     // can also give the prop a default value and make it required
     // this will throw an error in browser console if not followed
     const props = defineProps({
-     name: { type: String, default: 'Default Plan' },
+     name: { type: String, default: 'Default Plan', validator(value) {
+        return value.startsWith("The")
+     } },
      selected: { type: Boolean, default: false }
     //  price: { type: Number, required: true }
     })
 
-    const emit = defineEmits(['selected']) // define a custom event with defineEmits, returns an emit function which is saved in a const
+    // const emit = defineEmits(['selected']) // define a custom event with defineEmits, returns an emit function which is saved in a const
+    // to validate events need to define emits as an object, not an array
+    const emit = defineEmits({
+        hiThere: null, // an event which doesn't need validating
+        selected(payload) {
+            return typeof payload === 'string'
+        }
+    })
 
     function selectPlan() {
+        // validation above makes sure that props.name is a string
         emit('selected', props.name) // use saved emit function to emit an event called selected and send a payload to parent
     }
 </script>
